20bfab5cdbba028fd9830ea9cb8643a6c8d1a0b8
Tasnim Hasan Taz <tasnimtaz976@gmail.com>
2026-01-24 03:12:04 +0600

    Global chat relogin issue fixed

--- server.js
+++ server.js
@@ -205,6 +205,7 @@ io.on('connection', (socket) => {
       message,
       timestamp: data.timestamp || new Date().toISOString(),
       socketId: socket.id,
+      userId: data.userId || socket.id, // Use userId for persistent identification
       replyTo: data.replyTo || null
     };
 
